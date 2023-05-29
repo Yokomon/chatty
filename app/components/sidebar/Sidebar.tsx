140be788feec6859b0c1598a02266961cc5be8f6
yokomon <marowmars@gmail.com>
2023-05-29 17:38:08 +0100

    Components: ActiveStatus for channel list

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,12 +1,18 @@
+import { getConversations } from "@/app/actions/getConversations";
 import { getCurrentUser } from "@/app/actions/getCurrentUser";
+
+import { ActiveStatus } from "../ActiveStatus";
 import { DesktopSidebar } from "./DesktopSidebar";
 import { MobileFooter } from "./MobileFooter";
 
 export const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
+  const conversations = await getConversations();
+
   return (
     <div className="h-full">
-      <DesktopSidebar currentUser={currentUser!} />
+      <ActiveStatus />
+      <DesktopSidebar currentUser={currentUser!} conversations={conversations} />
       <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
