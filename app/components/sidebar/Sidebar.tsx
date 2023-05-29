492e215d6be2970fcc0df48342581a7bd1944474
yokomon <marowmars@gmail.com>
2023-05-29 18:16:23 +0100

    💚 Fix

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ export const Sidebar = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <ActiveStatus />
-      <DesktopSidebar currentUser={currentUser!} conversations={conversations} />
+      <DesktopSidebar currentUser={currentUser!} />
       <MobileFooter />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
